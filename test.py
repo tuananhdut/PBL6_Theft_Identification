c0d5340b8efef390096d3c27ef89864b67721313
tuananhdut <tuanhaoggg@gmail.com>
2024-11-15 02:37:23 +0700

    using thread in stream delay 2s

--- test.py
+++ test.py
@@ -1,24 +1,21 @@
-from picamera2 import Picamera2
+import threading
+import time
 import subprocess
 import cv2
 import datetime
 import pytz
+from picamera2 import Picamera2
 
-
-# Kh?i t?o d?i tu?ng Picamera2
 picam2 = Picamera2()
 
-# C?u hình camera v?i d?nh d?ng YUV420
 config = picam2.create_video_configuration(main={"size": (640, 480), "format": "YUV420"})
 picam2.configure(config)
-# picam2.set_controls({"FrameDurationLimits": (33333, 33333)})  # Tuong duong v?i 30 FPS
-picam2.set_controls({"FrameDurationLimits": (66666, 66666)}) 
+picam2.set_controls({"FrameDurationLimits": (66666, 66666)})  # 15 FPS
 picam2.start()
 
 rtmp_url = "rtmp://192.168.46.198/live/stream1"
 
 timezone = pytz.timezone('Etc/GMT-7')
-# ổn định nhất độ trễ tầm 6s
 ffmpeg_cmd = [
     'ffmpeg',
     '-y',
@@ -30,13 +27,14 @@ ffmpeg_cmd = [
     '-i', '-',
     '-c:v', 'libx264',
     '-preset', 'ultrafast',
-    '-tune', 'zerolatency',  # Gi?m d? tr? cho FFmpeg
-    '-b:v', '1000k',         # Bitrate cho video
-    '-bufsize', '500k',      # Gi?m kích thu?c b? d?m
+    '-tune', 'zerolatency',
+    '-b:v', '1000k',
+    '-bufsize', '500k',
     '-f', 'flv',
     rtmp_url
 ]
 
+# Tạo process để truyền dữ liệu qua FFmpeg
 process = subprocess.Popen(ffmpeg_cmd, stdin=subprocess.PIPE)
 
 def draw_datetime_to_frame(frame):
@@ -56,27 +54,32 @@ def draw_datetime_to_frame(frame):
     cv2.putText(frame, current_time, (text_x, text_y), font, font_scale, font_color, font_thickness, cv2.LINE_AA)
     return frame
 
-try:
+def write_to_ffmpeg():
     while True:
-        # Ch?p m?t khung hình t? camera
         frame = picam2.capture_array("main")
         frame = draw_datetime_to_frame(frame)
+        process.stdin.write(frame.tobytes())  # Ghi dữ liệu vào FFmpeg
 
-        # G?i d? li?u khung hình d?n FFmpeg qua pipe stdin
-        process.stdin.write(frame.tobytes())
+def main():
+    thread = threading.Thread(target=write_to_ffmpeg, daemon=True)
+    thread.start()
 
-        # Hi?n th? khung hình b?ng OpenCV (tùy ch?n)
-        # cv2.imshow('Camera', frame)
+    try:
+        while True:
+            frame = picam2.capture_array("main")
+            frame = draw_datetime_to_frame(frame)
+            # cv2.imshow('Camera', frame)
 
-        # Thoát khi nh?n 'q'
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+            # Thoát khi nhấn 'q'
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+    except KeyboardInterrupt:
+        print("Stream ended.")
+    finally:
+        picam2.close()
+        process.stdin.close()
+        process.wait()
+        cv2.destroyAllWindows()
 
-except KeyboardInterrupt:
-    print("K?t thúc stream.")
-
-finally:
-    picam2.close()
-    process.stdin.close()
-    process.wait()
-    cv2.destroyAllWindows()
+if __name__ == "__main__":
+    main()
